53b36fe930649ef8fd38dbe7ab1f3fda1c6609a5
Filipe Sousa <sousaafilp@gmail.com>
2022-06-27 05:41:15 -0300

    código processedural que será passado para poo

---
+++ Src/selecao/selecao.py
@@ -0,0 +1,49 @@
+from Src.Aluno.Aluno import Aluno
+from Src.Bolsa.Notas import Notas
+from Src.Professor.Professor import Professor
+
+import os
+
+
+class Selecao:
+
+    def __init__(self):
+        self._banco_matriculas = []
+        self._banco_matriculas_professores = []
+        self._banco_nomes = []
+        self._banco_nomes_professores = []
+        self._banco_notas_alunos = []
+        self._banco_notas_alunos2 = []
+        self._banco_status_aluno = []
+        self._banco_historico = []
+        self._banco_edital = []
+        self._banco_nota_corte = []
+
+    def cadastrar_professor(self):
+        pass
+
+    def cadastrar_aluno(self):
+        pass
+
+    def comparar_notas(self):
+        pass
+
+    def get_login_professor(self):
+        pass
+
+    def get_login_aluno(self):
+        pass
+
+    def set_historico(self):
+        pass
+
+    def mostrar_arquivos_alunos(self):
+        pass
+
+    def classificado(self):
+        pass
+
+    def visualizar_editais(self):
+        pass
+
+    def
